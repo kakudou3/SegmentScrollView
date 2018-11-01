0aaf481ad264270806314dea2874678d6466c556
Unknown <unknown@example.com>
2018-11-01 12:02:50 +0900

    [fix] レイアウト試行

--- SegmentScrollView/ViewController.swift
+++ SegmentScrollView/ViewController.swift
@@ -25,10 +25,29 @@ class ViewController: UIViewController {
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
+        
+        constrain(self.mainScrollView) {
+            $0.width == $0.superview!.width
+            $0.edges == $0.superview!.edges
+        }
+        
+        constrain(self.contentView) {
+            $0.edges == $0.superview!.edges
+        }
+        
+        constrain(self.headerContentsView) {
+            $0.height == 1000
+            $0.width == 100
+            $0.top == $0.superview!.top
+            $0.bottom == $0.superview!.bottom
+        }
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        
+        print(self.mainScrollView.frame)
+        print(self.contentView.frame)
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,36 +59,42 @@ extension ViewController {
     func setUpView() {
         
         // main scroll view
-        self.mainScrollView.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height)
+        //self.mainScrollView.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height)
         self.mainScrollView.backgroundColor = UIColor.green.withAlphaComponent(0.1)
         self.mainScrollView.isScrollEnabled = true
         self.mainScrollView.alwaysBounceVertical = true
         self.mainScrollView.isExclusiveTouch = true
         self.mainScrollView.delaysContentTouches = true
         self.mainScrollView.showsVerticalScrollIndicator = true
+        self.mainScrollView.delegate = self
         self.view.addSubview(self.mainScrollView)
         
         // content view
-        self.contentView.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height)
-        self.contentView.backgroundColor = UIColor.red.withAlphaComponent(0.2)
+        self.contentView.backgroundColor = UIColor.red.withAlphaComponent(0.8)
         self.mainScrollView.addSubview(self.contentView)
         
         // header contents view
-        self.headerContentsView.backgroundColor = UIColor.green.withAlphaComponent(0.2)
+        self.headerContentsView.backgroundColor = UIColor.green
         self.contentView.addSubview(self.headerContentsView)
         
         // tablev view
-        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
-        self.tableView.delegate = self
-        self.tableView.dataSource = self
-        self.tableView.showsVerticalScrollIndicator = true
-        self.contentView.addSubview(self.tableView)
+        //self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        //self.tableView.delegate = self
+        //self.tableView.dataSource = self
+        //self.tableView.showsVerticalScrollIndicator = true
+        //self.contentView.addSubview(self.tableView)
     }
 }
 
 extension ViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView == self.mainScrollView {
+            print(scrollView.contentOffset.y)
+        }
         
+        if scrollView == self.tableView {
+            print(scrollView.contentOffset.y)
+        }
     }
 }
 

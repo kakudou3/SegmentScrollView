15a1de954466816ea13b767fbba4b262a140564e
Unknown <unknown@example.com>
2018-11-03 10:00:34 +0900

    [add] 画像読み込みの検証

--- SegmentScrollView/ViewController.swift
+++ SegmentScrollView/ViewController.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 import Cartography
+import RxSwift
+import RxCocoa
+import Nuke
+import RxNuke
 
 class ViewController: UIViewController {
     
@@ -16,6 +20,8 @@ class ViewController: UIViewController {
 
     let contentView: UIView = .init(frame: .zero)
     let headerContentsView: UIView = .init(frame: .zero)
+    
+    let disposeBag: DisposeBag = .init()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -83,6 +89,39 @@ extension ViewController {
         //self.tableView.dataSource = self
         //self.tableView.showsVerticalScrollIndicator = true
         //self.contentView.addSubview(self.tableView)
+        
+        self.imageLoad()
+    }
+    
+    private func imageLoad() {
+        var configuration: URLSessionConfiguration {
+            let urlCache = DataLoader.sharedUrlCache
+            let conf = URLSessionConfiguration.default
+            
+            conf.urlCache = URLCache(memoryCapacity: urlCache.memoryCapacity, diskCapacity: urlCache.diskCapacity, diskPath: "com.github.kean.Nuke.Cache")
+            conf.requestCachePolicy = .returnCacheDataElseLoad
+            return conf
+        }
+        
+        let pipeline = ImagePipeline {
+            $0.dataLoader = DataLoader(configuration: configuration)
+        }
+        
+        ImagePipeline.shared = pipeline
+        
+        let imageView = UIImageView(frame: .zero)
+        self.view.addSubview(imageView)
+        
+        constrain(imageView) {
+            $0.height == 100
+            $0.width == 100
+            $0.top == $0.superview!.top
+            $0.left == $0.superview!.left
+        }
+        
+        let urlString = "https://pbs.twimg.com/profile_images/723852223252328448/-v6zm-YP_400x400.jpg"
+        
+        ImagePipeline.shared.rx.loadImage(with: URL(string: urlString)!).map({ $0.image }).asObservable().bind(to: imageView.rx.image).disposed(by: self.disposeBag)
     }
 }
 
@@ -117,3 +156,24 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         return cell
     }
 }
+
+// キャッシュをHTTP Headerのmax ageで管理できるように
+enum HttpDiskCache {
+    struct CacheControl {
+        var maxAge: Int?
+    }
+    
+    struct Header {
+        var cacheControl: CacheControl?
+        var date: Date?
+        var expires: Date?
+    }
+}
+
+extension HTTPURLResponse {
+    var header: HttpDiskCache.Header {
+        return HttpDiskCache.Header()
+    }
+}
+
+
